f7939410557283d01d510185ee5536cef6821511
mychelle-assis <mychelle.assis@gmail.com>
2020-08-09 15:09:59 -0300

    skdfjg

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,14 @@
 /*global jQuery:false */
+$(document).scroll(function(e){
+    var scrollTop = $(document).scrollTop();
+    if(scrollTop > 0){
+        console.log(scrollTop);
+        $('.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+    } else {
+        $('.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+    }
+});
+
 jQuery(document).ready(function($) {
 "use strict";
 
